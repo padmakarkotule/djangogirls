ef6555f0c689b369c0ace77186736a9bf4990023
Padmakar Kotule <padmakar.kotule@gmail.com>
2019-08-05 17:10:38 +0530

    updated code with draft, authentication security (@login decorator)

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -8,5 +8,14 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'text')
 
+
+# Form example with form.Form (e.g. create form without ModelForm.
 # class NameForm(forms.Form):
 # your_name = forms.CharField(label='Your name', max_length=100)
+
+
+# t
+
+class Testform(forms.Form):
+    your_fname = forms.CharField(label='Your First name', max_length=25)
+    your_lname = forms.CharField(label='Your Last name', max_length=35)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,27 +2,31 @@ from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, Testform
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+@login_required
 def post_detail(request,pk):
     # post = Post.objects.get(pk=pk)
     post = get_object_or_404(Post,pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -30,6 +34,7 @@ def post_new(request):
         return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -37,10 +42,44 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
 
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+def testform(request):
+    if request.method == "POST":
+        form = Testform(request.POST)
+        if form.is_valid():
+            form = form.cleaned_data
+            #data = form
+            #data = form.save(commit=False)
+            #your_fname = request.your_fname
+            # post.published_date = timezone.now()
+            #data.save()
+            return render(request, 'blog/testform_list.html', {'form':form})
+    else:
+        form = Testform()
+        return render(request, 'blog/testform.html',{'form': form})
